f7506caa033775e969ca2a2608da18cadfd28918
viniciusbombonato <viniciusm.bombonato@gmail.com>
2025-07-26 14:29:49 -0300

    finaly working

--- app.py
+++ app.py
@@ -4,4 +4,8 @@ app = Flask("__name__")
 
 @app.route("/")
 def homepage():
-    return render_template("layout.html")
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

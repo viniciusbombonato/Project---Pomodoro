5efcc9455a76b199b52f258ece7cd99f64f40d55
viniciusbombonato <viniciusm.bombonato@gmail.com>
2025-07-29 18:41:35 -0300

    home done, index working, timer to do

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask("__name__")
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def homepage():
+    if request.method == "POST":
+       minutes = request.form.get("minutes")
+       interval = request.form.get("interval")  
+
+       if minutes.isdigit() and interval.isdigit():
+            minutes = int(minutes)
+            interval = int(interval)
+
+            if minutes > 0 and interval > 0:
+                return render_template("index.html", minutes=minutes, interval=interval)
+
+        
+
+
     return render_template("home.html")
 
 

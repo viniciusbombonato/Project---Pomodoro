76f7c9c4633153dd09ac65c233070e607e18cb7b
Vinícius <viniciusm.bombonato@gmail.com>
2025-09-07 17:17:46 -0300

    the of the origin, starting the countdown js

--- app.py
+++ app.py
@@ -1,25 +1,21 @@
 from flask import Flask, render_template, request
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def homepage():
     if request.method == "POST":
-       minutes = request.form.get("minutes")
-       interval = request.form.get("interval")  
+        minutes = request.form.get("minutes")
+        interval = request.form.get("interval")  
 
-       if minutes.isdigit() and interval.isdigit():
+        if minutes and interval and minutes.isdigit() and interval.isdigit():
             minutes = int(minutes)
             interval = int(interval)
 
-            if minutes > 0 and interval > 0:
+            if 0 < minutes <= 60 and 0 < interval <= 60:
                 return render_template("index.html", minutes=minutes, interval=interval)
 
-        
-
-
     return render_template("home.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)

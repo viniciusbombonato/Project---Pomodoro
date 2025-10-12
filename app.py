2a5f3a52d31f3eaf6fbf6231c6c8e927e2b90993
Vinícius <viniciusm.bombonato@gmail.com>
2025-10-12 09:30:34 -0300

    changes in the host to  0.0.0.0

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import logging
+import os
 
 app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
@@ -34,4 +35,5 @@ def homepage():
         return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
